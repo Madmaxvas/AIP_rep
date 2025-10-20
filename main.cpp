13478844be0cb856eb6edf85a586442e7927dda2
Madmaxvas <maxmadvas@gmail.com>
2025-10-20 12:41:05 +0300

    Create and remove matrix

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+
+int** create(size_t rows, size_t cols);
+void remove(int** m, size_t rows, size_t cols);
+
 int main(){
 
 size_t rows = 0, cols = 0;
@@ -9,6 +13,11 @@ if(!std::cin){
 return 1;
 }
 
+int **m = create(rows,cols);
+
 std::cout << rows << " " << cols;
 
-std::cout << "\n";}
+std::cout << "\n";
+
+remove(m,rows,cols);
+}
