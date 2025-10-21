d4199e3d69621d4e8c06d7ac806b025b80dc6c93
Madmaxvas <maxmadvas@gmail.com>
2025-10-21 19:16:44 +0300

    Task complite

--- main.cpp
+++ main.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
 
-int** create(size_t rows, size_t cols){
-	int** result = new int*[rows];
-	size_t i = 0;
-	try{
-		for(size_t i = 0; i < rows; ++i){
-			result[i] = new int[cols];}}
-	catch(){
-	remove(result,i);
-	throw();}
-	return result;}
+void remove(int** m, size_t rows);
 
-void remove(int** m, size_t rows, size_t cols){
-	for(size_t i = 0; i < rows; ++i){	
-		delete [] m[i];}
-	delete []m;}
+int** create(size_t rows, size_t cols){
+	int** result = new (std::nothrow) int*[rows];
+	if (result == nullptr) {
+        return nullptr;
+    }
+	size_t i = 0;
+	for(i = 0; i < rows; ++i){
+		result[i] = new (std::nothrow) int[cols];
+        if (result[i] == nullptr) {
+	        remove(result, i);
+            return nullptr;
+        }
+    }
+	return result;
+}
+
+void remove(int** m, size_t rows){
+    if (m == nullptr) return;
+	for(size_t i = 0; i < rows; ++i){
+        if (m[i] != nullptr) {
+		    delete [] m[i];
+        }
+    }
+	delete [] m;
+}
 
 void input(int** m, size_t rows, size_t cols){
 	for (size_t i = 0; i < rows && (std::cin); ++i){
@@ -22,28 +34,41 @@ void input(int** m, size_t rows, size_t cols){
 			std::cin >> m[i][j];}}}
 
 void output(const int * const * m, size_t rows, size_t cols){
-	std::cout << rows << " " << cols;}
+	std::cout << cols << " " << rows << "\n";
+    	for (size_t j = 0; j < cols; ++j) {
+        	for (size_t i = 0; i < rows; ++i) {
+            		std::cout << m[i][j] << (i == rows - 1 ? "" : " ");
+        		}
+        	std::cout << "\n";
+    		}
+}
 
 int main(){
 
-size_t rows = 0, cols = 0;
+    size_t rows = 0, cols = 0;
 
-std::cin >> rows >> cols;
+    std::cin >> rows >> cols;
 
-if(!std::cin){
-return 1;}
+    if(!std::cin){
+        return 1;
+    }
 
-int **m = create(rows, cols);
+    int **m = nullptr;
 
-input(m, rows, cols);
+    m = create(rows, cols);
 
-if(!std::cin){
-remove(m,rows,cols);
-return 1;}
+    if (m == nullptr) {
+        return 2;
+    }
 
-output(m, rows, cols);
+    input(m, rows, cols);
 
-remove(m,rows,cols);
+    if(!std::cin){
+        remove(m, rows);
+        return 1;
+    }
 
-std::cout << "\n";
+    output(m, rows, cols);
+    remove(m, rows);
+    return 0;
 }
